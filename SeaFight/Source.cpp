8812fa11efc71827a3462f39264b1a35476d0774
Yaroslav Lukyanov <lukuanov0703yaroslav@gmail.com>
2021-03-10 01:05:35 +0300

    Чё-то набросал как бы, ну вот

--- SeaFight/Source.cpp
+++ SeaFight/Source.cpp
@@ -1,12 +1,121 @@
 #include <iostream>;
+#include <Windows.h>;
+using namespace std;
+
+void SinglePlayer(),
+	 MultiPlayer(),
+	 Settings(),
+	 Exit();
+
+struct Options {
+	//Пустое поле боя
+	int Map[20][20] = { {1,1,1,1,1,1,1,1,1,1,1,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,0,0,0,0,0,0,0,0,0,0,1, },
+						{1,1,1,1,1,1,1,1,1,1,1,1, } };
+
+};
 
 int ShowMenu() {
 	// TODO: Menu
+	do {
+		//Главное меню
+		system("cls");
+		cout << "\n\n\t\t\t\t\tДобро пожаловать в 'Морской бой!'\n\n"
+			"\t\t\t[1]. Одиночная игра\n"
+			"\t\t\t[2]. Многопользовательская игра\n"
+			"\t\t\t[3]. Настройки\n"
+			"\t\t\t[4]. Выход\n";
+		int MainMenuOption;
+		cout << "\n\t\t\t[?] - "; cin >> MainMenuOption;
+		switch (MainMenuOption)
+		{
+		case 1:
+			SinglePlayer();
+			break;
+		case 2:
+			MultiPlayer();
+			break;
+		case 3:
+			Settings();
+			break;
+		case 4:
+			Exit();
+			break;
+		default:
+			break;
+		}
+	} while (true);
 	// SinglePlayer
 	// Player vs Player
 	// Settings (Map size, ships color, manual or randomed placement)
 	// Exit
 }
+
+void ShowMap(Options& options) {
+	//Отрисовка карта в зависимости от значения в массиве
+	for (int i = 0; i < 12; i++) {
+		for (int j = 0; j < 12; j++) {
+			//Если... то стена
+			if (options.Map[i][j] == 1) {
+				cout << '#' << " ";
+			}
+			//Если... то игрок
+			else if (options.Map[i][j] == 2) {
+				cout << '*' << " ";
+			}
+			//Если... то ничего
+			else if (options.Map[i][j] == 0) {
+				cout << '~' << " ";
+			}
+			//Если... то мимо
+			else if (options.Map[i][j] == 3) {
+				cout << 'o' << " ";
+			}
+			//Если... то попадание
+			else if (options.Map[i][j] == 4) {
+				cout << 'x' << " ";
+			}
+			//Если... то уничтожен
+			else if (options.Map[i][j] == 5) {
+				cout << 'X' << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+
+//Блок методов для главного меню
+void SinglePlayer() {
+	Options options;
+	system("cls");
+	cout << "\n\n\t\t\t\t\tВы выбрали режим одиночной игры\n\n";
+	ShowMap(options);
+	system("pause");
+}
+void MultiPlayer() {
+	cout << "Вы выбрали режим многопользовательской игры";
+}
+void Settings() {
+	cout << "Вы зашли в настройки";
+}
+void Exit() {
+	system("cls");
+	cout << "Выход из игры...\n";
+	exit(0);
+}
+
+
+
 
 void PlayerMove() {
 
@@ -18,5 +127,7 @@ void BotMove() {
 
 int main() {
 	// TODO: Sea Fight
+	setlocale(0, "");
+	Options options;
 	ShowMenu();
 }
