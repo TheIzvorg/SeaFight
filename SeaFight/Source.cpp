00e36a922cf929bd3ca485b9f9ee3525cbca30d2
TheIzvorg <TheIzvorg@users.noreply.github.com>
2021-03-09 19:26:20 +0300

    Добавил методы-пустышки, чтобы немного систематизировать названия

--- SeaFight/Source.cpp
+++ SeaFight/Source.cpp
@@ -1,5 +1,22 @@
 #include <iostream>;
 
+int ShowMenu() {
+	// TODO: Menu
+	// SinglePlayer
+	// Player vs Player
+	// Settings (Map size, ships color, manual or randomed placement)
+	// Exit
+}
+
+void PlayerMove() {
+
+}
+
+void BotMove() {
+
+}
+
 int main() {
 	// TODO: Sea Fight
+	ShowMenu();
 }
