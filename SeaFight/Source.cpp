d4e5bfa41ee8094796e54efd4f80c23eee5d76bf
TheIzvorg <izvorg@yandex.ru>
2021-03-12 14:51:19 +0300

    Добавил перемещение всех палуб за раз + прототип разворота коробля, который пока не работает.

--- SeaFight/Source.cpp
+++ SeaFight/Source.cpp
@@ -16,7 +16,7 @@ struct Options {
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
-						{1,0,0,0,2,2,2,2,0,0,0,1, },
+						{1,0,0,0,9,9,9,9,0,0,0,1, },
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
 						{1,0,0,0,0,0,0,0,0,0,0,1, },
@@ -28,7 +28,7 @@ struct Options {
 		shipL = 2,
 		shipXL = 1;
 
-	int PlayerX = 6, PlayerY = 4;
+	int PlayerX = 4, PlayerY = 6;
 
 	// Тест
 
@@ -92,11 +92,11 @@ void ShowMap(Options& options) {
 				SetColor('#', 14);
 			}
 			//Если... то игрок
-			else if (options.Map[i][j] == 2) {
+			else if (options.Map[i][j] == 2 || options.Map[i][j] == 9) {
 				SetColor('=', 7);
 			}
 			//Если... то ничего
-			else if (options.Map[i][j] == 0) {
+			else if (options.Map[i][j] == 0 || options.Map[i][j] == 8) {
 				SetColor('~', 9);
 			}
 			//Если... то мимо
@@ -119,25 +119,133 @@ void ShowMap(Options& options) {
 
 void PlaceShip(Options& options) {
 	bool isStop = false;
+	// Массив координат, в котором можно будет увидеть координаты корабля
+	int cord[4][2];
+	for (int x = 0, count = 0; x < 12; x++) {
+		for (int y = 0; y < 12; y++) {
+			// Число 9 на карте означает корабль, с которым прямо сейчас происходит взаимодействие
+			// После того как будет выбрано место и нажата определённая кнопка
+			// Все 9-ки на поле превратятся в 2, чтобы случайно не задеть их
+			if (options.Map[y][x] == 9) {
+				// Заполняю массив координатами
+				// Count - это номер палубы, когда 0 отвечает за координату X, а 1 за Y
+				cord[count][0] = x;
+				cord[count][1] = y;
+				count++;
+				if (count == 4) {
+					break;
+				}
+			}
+			else {
+			}
+		}
+		if (count == 4) {
+			break;
+		}
+	}
 	while (!isStop) {
 		switch (_getch())
 		{
-		case 'A':
-		case 'a':
-		case 75:
-			if (options.Map[options.PlayerX - 1][options.PlayerY] != 1) {
-				options.PlayerX--;
-				isStop = true;
-				for (int i = 0; i <= 4; i++) {
-					if (options.Map[options.PlayerX][options.PlayerY + i] != 2) {
-						options.Map[options.PlayerX][options.PlayerY + i] = 0;
-						
+			case 'A':
+			case 'a':
+			case 75:
+				// Как проверку я использую координаты крайных палуб карабля
+				// Пока такая проверка настроенна только на 4-х палубники, но после это исправлю
+				if (options.Map[cord[3][1]][cord[3][0] - 1] != 1 && options.Map[cord[0][1]][cord[0][0] - 1] != 1) {
+					for (int i = 0; i < 4; i++) {
+						// Абсолютно всё перемещение по карте производится по следующей схеме:
+						// Сначала убираю все 9-ки с карты
+						options.Map[cord[i][1]][cord[i][0]] = 0;
+						// Изменяю координаты всех палуб (в данном случае палубы перемещаются влево по X)
+						cord[i][0]--;
+						// А потом возвращаю 9-ки на карту используя изменившиеся координаты палуб
+						options.Map[cord[i][1]][cord[i][0]] = 9;
 					}
 				}
-			}
-			break;
+				/*
+					// Я отказался от этого метода т.к. он оказался не эффективным.
+				if (options.Map[options.PlayerY][options.PlayerX-1] != 1) {
+					options.PlayerX--;
+					for (int i = 1; i <= 4; i++) {
+						if (options.Map[options.PlayerY][options.PlayerX + i + 1] != 2) {
+							options.Map[options.PlayerY][options.PlayerX + i] = 0;
+							break;
+						}
+					}
+				}*/
+				isStop = true;
+				break;
+			case 'D':
+			case 'd':
+			case 77:
+				if (options.Map[cord[3][1]][cord[3][0] + 1] != 1 && options.Map[cord[0][1]][cord[0][0] + 1] != 1) {
+					for (int i = 3; i >= 0; i--) {
+						options.Map[cord[i][1]][cord[i][0]] = 0;
+						cord[i][0]++;
+						options.Map[cord[i][1]][cord[i][0]] = 9;
+					}
+				}
+				isStop = true;
+				break;
+			case 'S':
+			case 's':
+			case 80:
+				if (options.Map[cord[3][1]+1][cord[3][0]] != 1 && options.Map[cord[0][1]+1][cord[0][0]] != 1) {
+					for (int i = 3; i >= 0; i--) {
+						options.Map[cord[i][1]][cord[i][0]] = 0;
+						cord[i][1]++;
+						options.Map[cord[i][1]][cord[i][0]] = 9;
+					}
+				}
+				isStop = true;
+				break;
+			case 'W':
+			case 'w':
+			case 72:
+				if (options.Map[cord[3][1] - 1][cord[3][0]] != 1 && options.Map[cord[0][1] - 1][cord[0][0]] != 1) {
+					for (int i = 0; i < 4; i++) {
+						options.Map[cord[i][1]][cord[i][0]] = 0;
+						cord[i][1]--;
+						options.Map[cord[i][1]][cord[i][0]] = 9;
+					}
+				}
+				isStop = true;
+				break;
+			case 'E':
+			case 'e':
+			case 'Q':
+			case 'q':
+				// Отсюда начинается костыльно-адский код для разворота фигуры
+				if (true /*Проверка пока не сделана*/) {
+					// Массив-буфер созданный с целью облегчить разворот фигуры
+					int	bufferArray[4][4];
+					for (int i = 0; i < 4; i++) {
+						for (int j = 0; j < 4; j++) {
+							// Заполняю этот массив следующим образом:
+							// Используя координаты палуб я забираю с карты кусок 4 на 4, где находится корабль
+							bufferArray[i][j] = options.Map[cord[i][0] + i][cord[j][1] + j % 2];
+						}
+						// И убираю все палубы с карты
+						options.Map[cord[i][1]][cord[i][0]] = 0;
+					}
+					int buf[4][4];
+					for (int i = 0; i < 4; i++){
+						for (int j = 0; j < 4; j++) {
+							// Дальше просто разворачиваю массив просто меняя местами Y с X
+							buf[j][i] = bufferArray[i][j];
+						}
+					}
+					for (int i = 0; i < 4; i++) {
+						for (int j = 0; j < 4; j++) {
+							// И вывожу изменённые координаты палуб на карту
+							// К сожалению этот этап пока работает не верно.
+							options.Map[cord[i][1]][cord[j][0]] = buf[i][j];
+						}
+					}
+				}
+				isStop = true;
+				break;
 		}
-		options.Map[options.PlayerY][options.PlayerX] = 2;
 	}
 }
 
@@ -152,7 +260,7 @@ void BotMove() {
 void SinglePlayer() {
 	Options options;
 	cout << "\n\n\t\t\t\t\tВы выбрали режим одиночной игры\n\n";
-	for (int i = 0; i < 10 ; i++) {
+	for (int i = 0; i < 100 ; i++) {
 		system("cls");
 		ShowMap(options);
 		cout << "\nОсталось кораблей"
